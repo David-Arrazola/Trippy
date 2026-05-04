df97595bac0b54126c8fffc638f452233c3b6de9
David Arrazola <arrazola.dvd@gmail.com>
2026-05-04 16:56:51 -0400

    ADD form functionality. When submit, form prints a message

--- frontend/src/TripForm/TripForm.jsx
+++ frontend/src/TripForm/TripForm.jsx
@@ -1,41 +1,56 @@
-import {useState} from 'react';
+import { useState } from "react";
+import "./TripForm.css";
 
 export default function TripForm() {
-    const [formData, setFormData] = useState({
-        destination: '',
-        tripLengthL: '',
-        budget: '',
+  const [formData, setFormData] = useState({
+    destination: "",
+    tripLengthL: "",
+    budget: "",
+  });
+
+  const updateForm = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...TripForm,
+      [name]: value,
     });
+  };
 
-    const updateForm = (event) => {
-        const {name, value} = event.target;
-        setFormData({
-            ...TripForm,
-            [name]: value,
-        })
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("PRINTING");
+  };
 
-    return (
-        <form>
-            <label>
-                Primary Destination:
-                <input type="text" name="destination" 
-                value={formData.destination} 
-                onChange={() => updateForm}/>
-            </label>
-            <label>
-                Trip Length:
-                <input type="number" name="tripLength"
-                value={formData.tripLength}
-                onChange={() => updateForm}/>
-            </label>
-            <label>
-                Budget:
-                <input type="number" name="budget"
-                value={formData.budget}
-                onChange={() => updateForm}/>
-            </label>
-        </form>
-    );
-
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Primary Destination:
+        <input
+          type="text"
+          name="destination"
+          value={formData.destination}
+          onChange={() => updateForm}
+        />
+      </label>
+      <label>
+        Trip Length:
+        <input
+          type="number"
+          name="tripLength"
+          value={formData.tripLength}
+          onChange={() => updateForm}
+        />
+      </label>
+      <label>
+        Budget:
+        <input
+          type="number"
+          name="budget"
+          value={formData.budget}
+          onChange={() => updateForm}
+        />
+      </label>
+      <button type="submit">Create Iternary</button>
+    </form>
+  );
 }
