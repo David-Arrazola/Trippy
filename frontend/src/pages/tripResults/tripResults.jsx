ed1ed38db9e78009776075acf4939c5a655aacda
David Arrazola <arrazola.dvd@gmail.com>
2026-05-29 09:44:06 -0400

    ADD comments of TODOS for future tasks

--- frontend/src/pages/tripResults/tripResults.jsx
+++ frontend/src/pages/tripResults/tripResults.jsx
@@ -16,6 +16,10 @@ const mapContainerStyle = {
   height: "500px",
 };
 
+//TODO separate areas like "Hotels" and "Flights" into components in different pages and export them
+//TODO add a feature where user can carosell through hotels and flights so they can choose BUT also include RECOMMEND section where it displays what  the AI recommends. OR they can change to they choose from selection of switch via carosell
+//TODO In map make it so line colors switch per city and identify which city comes first, then second, then third
+
 export default function TripResults() {
   const { tripState } = useTrip();
   const navigate = useNavigate();
